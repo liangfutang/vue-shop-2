1aaa8ba5eb865762a35ddce467df4557f7295cbd
liangfutang <liangfu.tang@ecarx.com.cn>
2021-07-05 19:28:48 +0800

    全局css和简单的登录组件添加

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 登录组件
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  // 空路由的时候跳转到登录页面
+  {
+    path: '/',
+    redirect: '/login'
+  },
+
+  // 登录路由
+  {
+    path: '/login',
+    name: '登录',
+    component: Login
+  }
+]
 
 const router = new VueRouter({
   routes
