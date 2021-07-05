5ae26ff4cdd65567f5041a494d5e02b27c70848d
liangfutang <liangfu.tang@ecarx.com.cn>
2021-07-05 20:17:12 +0800

    设置全局样式及简单登录页

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="login_container">
-        登录组件
+        <div class="login_box">
+            <!-- 头像区域 -->
+            <div class="avatar_box">
+                <img src="" alt="登录表单上的头像图标">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,5 +14,13 @@ export default {}
 </script>
 
 <style lang="less" scoped>
+.login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+}
 
+.login_box{
+    width: 450px;
+    height: 300px;
+}
 </style>
