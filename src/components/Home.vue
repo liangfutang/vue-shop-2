5b9db9eefe14f207a20a855d978f6144e912f471
liangfutang <liangfu.tang@ecarx.com.cn>
2021-07-07 19:31:13 +0800

    完成左侧导航栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,35 +12,32 @@
             <!-- 侧边 -->
             <el-aside width="auto" @mouseenter.native="collapseOpen" @mouseleave.native="collapseClose">
                 <el-menu class="el-menu-vertical-demo" :collapse="isCollapse" background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+                    <!-- 导航--展示一 -->
                     <el-submenu index="1">
                         <template slot="title">
                             <i class="el-icon-location"></i>
-                            <span slot="title">导航一</span>
+                            <span slot="title">产品</span>
                         </template>
                         <el-menu-item-group>
-                            <span slot="title">分组一</span>
-                            <el-menu-item index="1-1">选项1</el-menu-item>
-                            <el-menu-item index="1-2">选项2</el-menu-item>
+                            <span slot="title">报表展示</span>
+                            <el-menu-item index="1-1">书籍类</el-menu-item>
                         </el-menu-item-group>
-                        <el-menu-item-group title="分组2">
-                            <el-menu-item index="1-3">选项3</el-menu-item>
+                        <el-menu-item-group title="数据录入">
+                            <el-menu-item index="1-3">录入</el-menu-item>
                         </el-menu-item-group>
-                        <el-submenu index="1-4">
-                            <span slot="title">选项4</span>
-                            <el-menu-item index="1-4-1">选项1</el-menu-item>
-                        </el-submenu>
                     </el-submenu>
-                    <el-menu-item index="2">
-                        <i class="el-icon-menu"></i>
-                        <span slot="title">导航二</span>
-                    </el-menu-item>
-                    <el-menu-item index="3" disabled>
-                        <i class="el-icon-document"></i>
-                        <span slot="title">导航三</span>
-                    </el-menu-item>
+                    <!-- 导航--展示二 -->
+                    <el-submenu index='2'>
+                        <template slot="title">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">利润</span>
+                        </template>
+                        <el-menu-item index="1-3">经销</el-menu-item>
+                    </el-submenu>
+                    <!-- 导航--设置 -->
                     <el-menu-item index="4">
                         <i class="el-icon-setting"></i>
-                        <span slot="title">导航四</span>
+                        <span slot="title">设置</span>
                     </el-menu-item>
                 </el-menu>
             </el-aside>
