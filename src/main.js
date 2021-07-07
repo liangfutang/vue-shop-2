095a89462cc8f4147d96f30fb33a013d4ecc511c
liangfutang <liangfu.tang@ecarx.com.cn>
2021-07-07 20:43:29 +0800

    除了登录接口外的所有接口都要添加token

--- src/main.js
+++ src/main.js
@@ -12,6 +12,16 @@ import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:7777'
 axios.defaults.timeout = 5000
+// 拦截请求，为所有请求添加token参数
+axios.interceptors.request.use(config => {
+  // 如果是登录页则不设置
+  if (config.url === '/api/private/v1/login') {
+    return config
+  }
+  // 其他请求都要添加token字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.axios = axios
 
 Vue.config.productionTip = false
