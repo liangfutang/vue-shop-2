ec87881bcb210086ada45209947845aa03d32dde
liangfutang <tangliangfucom@sina.com>
2021-07-12 01:32:18 +0800

    添加权限列表接口

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -14,7 +14,25 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // 权限列表数据
+      rightsList: []
+    }
+  },
+  created () {
+    this.getRightList()
+  },
+  methods: {
+    // 获取权限列表
+    async getRightList () {
+      const { data: rightsList } = await this.$http.get('/api/private/v1/right/list')
+      if (rightsList.meta.status !== 200) return this.$message.error(rightsList.meta.msg)
+      this.rightsList = rightsList.data
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
